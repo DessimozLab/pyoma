d42b0ef87fe2596e63e8d92b22169d194cb4b7bd
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2016-11-17 12:37:31 +0100

    fix test to work with new version of class

--- tests/browser/test_xrefs.py
+++ tests/browser/test_xrefs.py
@@ -9,22 +9,22 @@ from pyoma.browser import convert as pyoma
 
 class XRefParsingTest(unittest.TestCase):
     def setUp(self):
-        data = io.StringIO(
+        self.data = io.StringIO(
             """<E><ID>ENSG00000204640</ID><AC>ENSP00000366061; ENST00000376865</AC><DE>hypotetical protein</DE><GI>125233342</GI><UniProt/TrEMBL>P21522</UniProt/TrEMBL></E>
             <E><ID>BLA22; BLABLA22.Rep22</ID><AC>BLA22.1</AC><EntrezGene>32244</EntrezGene><PMP>P21122; Q24S32</PMP><GO>GO:0006270@[[IDA,{'PMID:2167836'}],[IEA,{'GO_REF:002','GO_REF:020','OMA_Fun:001'}]]; GO:0006275@[[IEA,{'GO_REF:002','GO_REF:020','OMA_Fun:001'}]]</GO></E>
             <E><UniProt/TrEMBL>L8ECQ9_BACSU</UniProt/TrEMBL><SwissProt_AC>Q6CI62</SwissProt_AC><SwissProt>ASF1_YARLI</SwissProt><ID>FBgn0218776</ID><AC>FBpp0245919; FBtr0247427</AC><DE>β-hemoglobin</DE><GO></GO></E>""")
-        self.db_parser = pyoma.DarwinDbEntryParser(data)
+        self.db_parser = pyoma.DarwinDbEntryParser()
         self.desc_manager = unittest.mock.Mock()
         self.importer = pyoma.XRefImporter(self.db_parser, None, None, self.desc_manager)
 
     def test_standard_handler(self):
-        self.db_parser.parse_entrytags()
+        self.db_parser.parse_entrytags(self.data)
         enum = pyoma.tablefmt.XRefTable.columns.get('XRefSource').enum
         self.assertIn((1, enum.GI, b'125233342',), self.importer.xrefs)
         self.assertIn((3, enum['UniProtKB/SwissProt'], b'ASF1_YARLI',), self.importer.xrefs)
 
     def test_multi_handler(self):
-        self.db_parser.parse_entrytags()
+        self.db_parser.parse_entrytags(self.data)
         enum = pyoma.tablefmt.XRefTable.columns.get('XRefSource').enum
         self.assertIn((2, enum.PMP, b'P21122',), self.importer.xrefs)
         self.assertIn((2, enum.PMP, b'Q24S32',), self.importer.xrefs)
@@ -35,18 +35,18 @@ class XRefParsingTest(unittest.TestCase):
             self.assertIsNotNone(match)
 
     def test_uniprot_ids(self):
-        self.db_parser.parse_entrytags()
+        self.db_parser.parse_entrytags(self.data)
         enum = pyoma.tablefmt.XRefTable.columns.get('XRefSource').enum
         self.assertIn((3, enum['UniProtKB/TrEMBL'], b'L8ECQ9',), self.importer.xrefs)
         self.assertIn((3, enum['UniProtKB/TrEMBL'], b'Q6CI62',), self.importer.xrefs)
 
     def test_go(self):
-        self.db_parser.parse_entrytags()
+        self.db_parser.parse_entrytags(self.data)
         self.assertIn((2, 6270, 'IDA', b'PMID:2167836'), self.importer.go)
         self.assertIn((2, 6275, 'IEA', b'OMA_Fun:001'), self.importer.go)
 
     def test_disambiguate(self):
-        self.db_parser.parse_entrytags()
+        self.db_parser.parse_entrytags(self.data)
         enum = pyoma.tablefmt.XRefTable.columns.get('XRefSource').enum
         self.assertIn((3, enum['FlyBase'], b'FBgn0218776'), self.importer.xrefs)
         self.assertIn((3, enum['FlyBase'], b'FBtr0247427'), self.importer.xrefs)
@@ -59,7 +59,7 @@ class XRefParsingTest(unittest.TestCase):
         self.assertIn((1, enum['Ensembl Transcript'], b'ENST00000376865'), self.importer.xrefs)
 
     def test_descriptions_passed_to_description_manager(self):
-        self.db_parser.parse_entrytags()
+        self.db_parser.parse_entrytags(self.data)
         self.assertEqual(len(self.desc_manager.add_description.call_args_list), 2)
         self.assertEqual((3,u'β-hemoglobin'), self.desc_manager.add_description.call_args[0])
 

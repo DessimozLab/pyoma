5ad6fb7571a1291e0b31629d1acffd89af6e5a3a
Alex Warwick Vesztrocy <alex@warwickvesztrocy.co.uk>
2017-07-04 16:39:06 +0100

    Anoter typo of db.filename -> self.db.filename

--- pyoma/browser/db.py
+++ pyoma/browser/db.py
@@ -439,7 +439,7 @@ class SequenceSearch(object):
         # Assume the index is stored in the main DB if there is no .idx file
         self.db = db.get_hdf5_handle()
         self.db_idx = (db if not os.path.isfile(self.db.filename + '.idx') else
-                       tables.open_file(db.filename + '.idx', 'r'))
+                       tables.open_file(self.db.filename + '.idx', 'r'))
 
         # Protein search arrays.
         self.seq_idx = self.db_idx.root.Protein.SequenceIndex

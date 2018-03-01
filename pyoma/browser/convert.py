fd9f4d9c2064f9775701fded6a6e70fc571098f7
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2018-03-01 11:08:15 +0100

    first pass on adding hog cnts to json

--- pyoma/browser/convert.py
+++ pyoma/browser/convert.py
@@ -1600,6 +1600,51 @@ class PfamDomainNameParser(CathDomainNameParser):
     source = b'Pfam'
 
 
+def augment_genomes_json_download_file(fpath, h5, backup='.bak'):
+    """Augment the genomes.json file in the download section with additional info
+
+    This function stores the ncbi taxonomy identifiers of internal nodes and adds
+    the number of ancestral genes to the internal nodes.
+
+    :param fpath: path to genomes.json file
+    :param h5: hdf5 database handle."""
+    # load nr of ancestral genomes at each level
+    ancestral_hogs = collections.Counter()
+    step = 2**15
+    hog_tab = h5.get_node('/HogLevel')
+    for start in range(0, len(hog_tab), step):
+        ancestral_hogs.update((l.decode() for l in hog_tab.read(start, stop=start+step, field='Level')))
+    # load taxonomy and sorter by Name
+    tax = h5.get_node('/Taxonomy').read()
+    sorter = numpy.argsort(tax['Name'])
+    with open(fpath, 'rt') as fh:
+        genomes = json.load(fh)
+
+    def traverse(node):
+        if 'children' not in node:
+            return
+        for child in node['children']:
+            traverse(child)
+        try:
+            node['nr_hogs'] = ancestral_hogs[node['name']]
+        except KeyError as e:
+            common.package_logger.warning('no ancestral hog counts for '+node['name'])
+            node['nr_hogs'] = 0
+
+        try:
+            n = node['name'].encode('utf-8')
+            idx = numpy.searchsorted(tax['Name'], n, sorter=sorter)
+            if tax['Name'][sorter[idx]] == n:
+                node['taxid'] = int(tax['NCBITaxonId'][sorter[idx]])
+            raise ValueError('not in taxonomy: {}'.format(n))
+        except Exception:
+            common.package_logger.exception('Cannot identify taxonomy id')
+
+    traverse(genomes)
+    with open(fpath+'.bak', 'wt') as fh:
+        json.dump(genomes, fh)
+
+
 def getDebugLogger():
     import logging
 

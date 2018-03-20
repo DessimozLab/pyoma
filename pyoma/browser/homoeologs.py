b6b6343cf4b9b417c1c52abb0dfef3f84bc6d67c
Natasha Glover <nattyglove@gmail.com>
2018-03-20 19:36:06 +0100

    load synteny from csv for confidence score, changed scaling of conf scores

--- pyoma/browser/homoeologs.py
+++ pyoma/browser/homoeologs.py
@@ -148,13 +148,25 @@ class HomeologsConfidenceCalculator(object):
         self.genome_df.reset_index(inplace=True)
         self.relations_df = self._load_pairwise_relations()
 
+
     def _load_pairwise_relations(self):
         """load the homoeologous relations of the cannonical splice variants only
         The method returns a pandas dataframe with the relations."""
         df = pandas.DataFrame(
             self.h5_handle.get_node('/PairwiseRelation/{}/within'.format(self.genome)).read_where('RelType == 5'))
         df = df[df['EntryNr1'].isin(self.genome_df['EntryNr']) & df['EntryNr2'].isin(self.genome_df['EntryNr'])]
-        return df[['EntryNr1', 'EntryNr2', 'SyntenyConservationLocal', 'Distance']]
+        df = df[['EntryNr1', 'EntryNr2', 'Distance','SyntenyConservationLocal']]
+
+        #added by NG to read synteny scores from file
+        df.rename(columns={'SyntenyConservationLocal': 'SyntenyConservationLocal_old'}, inplace=True)
+        tmp_df = pandas.read_csv('/Users/nglover/DessimozRepos/pyoma/pyoma/browser/'
+                             + self.genome + 'synteny.tsv', sep='\t', index_col=0)
+        df = pandas.merge(left=df, right=tmp_df, how='left', left_on=['EntryNr1','EntryNr2'],
+                     right_on=['entry_nr1','entry_nr2'])
+        df.rename(columns={'mean_synteny_score':'SyntenyConservationLocal'}, inplace=True)
+        ######
+
+        return df[['EntryNr1', 'EntryNr2', 'SyntenyConservationLocal', 'SyntenyConservationLocal_old','Distance']]
 
     def _count_homeologs_per_entry(self, df):
         return collections.Counter(df['EntryNr1'])
@@ -162,7 +174,7 @@ class HomeologsConfidenceCalculator(object):
     def _augment_dataframe_with_all_features(self, df):
         counts = self._count_homeologs_per_entry(df)
         df['TotalCopyNr'] = df.apply(lambda x: counts[x['EntryNr1']] + counts[x['EntryNr2']], axis=1)
-        df.iloc[df.SyntenyConservationLocal < 0, ['SyntenyConservationLocal']] = 0
+        #df.iloc[df.SyntenyConservationLocal < 0, ['SyntenyConservationLocal']] = 0
         return df
 
     def calculate_scores(self):
@@ -189,8 +201,8 @@ class HomeologsConfidenceCalculator(object):
 
         df['fuzzy_confidence'] = df.apply(defuzzify, axis=1)
 
-        # scale the confidence between 0 and 100
-        min_max_scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(0, 100))
+        # scale the confidence between minimum value and 100
+        min_max_scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(df['fuzzy_confidence'].min(), 100))
         df['fuzzy_confidence_scaled'] = min_max_scaler.fit_transform(df['fuzzy_confidence'].values.reshape(-1, 1))
         return df
 

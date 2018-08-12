c2bc15b84b23972da9beae3d77ce9d76806cbb75
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2018-08-12 22:28:09 +0200

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ shutil.copyfile('bin/importdata.py', 'bin/oma2hdf')
 
 setup(
     name=name,
-    version='0.6.0',
+    version='0.6.1',
     author='Adrian Altenhoff',
     author_email='adrian.altenhoff@inf.ethz.ch',
     description='todoc',

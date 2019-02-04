cf11a4ecae10127e3bc37f22927c09b3d60ab1f9
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2019-02-04 13:49:58 +0100

    allow shorter jobs with auto restart for seq2hog mapping

--- bin/map_seqs_to_hogs.py
+++ bin/map_seqs_to_hogs.py
@@ -7,14 +7,22 @@ import Bio.SeqIO
 import logging
 import csv
 import hashlib
+import sys
+import signal
 logger = logging.getLogger('hog_mapper')
 
+
+def handler(signum, frame):
+    logger.info('signal handler called with signal {}. Raise a KeybordInterupt Exception'.format(signum))
+    raise KeyboardInterrupt("time is up")
+
+signal.signal(signal.SIGUSR2, handler)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Map sequences to HOGs")
     parser.add_argument('hdf5', help="Path to the hdf5 database")
-    parser.add_argument('fasta', nargs="+", help="File(s) with fasta formatted protein sequences")
+    parser.add_argument('fasta', nargs="+", help="File(s) with fasta formatted protein sequences. Checkpointing does only work with a single input file so far.")
     parser.add_argument('--out', default="map2hog.txt",
                         help="filename output file. Will be modified if run with more than one process")
     parser.add_argument('-n', '--nr_proc', type=int, help="Nr of processes to use")
@@ -32,22 +40,43 @@ if __name__ == "__main__":
     db = pyoma.browser.db.Database(conf.hdf5)
     hog_mapper = pyoma.browser.db.SimpleSeqToHOGMapper(db)
 
-    with open(outfn, 'wt') as fout:
+    last_id = None
+    if os.path.exists(outfn+'.ckpt'):
+        with open(outfn+".ckpt") as fh:
+            last_id = fh.read()
+
+    with open(outfn, 'at') as fout:
         csv_writer = csv.writer(fout, delimiter="\t")
-        if pInf.this_proc_nr == 1:
+        if last_id is None and pInf.this_proc_nr == 1:
             csv_writer.writerow(['query', 'target', 'is_main_isoform', 'HOG', 'PAM_distance', 'Alignment_score'])
             
         for infn in conf.fasta:
             with open(infn, 'rt') as fin:
                 seqs = Bio.SeqIO.parse(fin, 'fasta')
                 myjobs = filter(lambda srec: pInf.is_my_job(srec.id), seqs)
-                it = hog_mapper.imap_sequences(myjobs)
-
-                for map_res in it:
-                    if map_res.target.hog_family_nr != 0:
-                        hog_id = db.format_hogid(map_res.target.hog_family_nr)
-                    else:
-                        hog_id = "n/a"
-                    csv_writer.writerow([map_res.query, map_res.target.omaid,
-                                         map_res.target.entry_nr == map_res.closest_entry_nr,
-                                         hog_id, map_res.distance, map_res.score])
+                # forward in case we have a 
+                if last_id is not None:
+                    for xx in myjobs:
+                        if xx.id == last_id:
+                            break
+                try:
+                    it = hog_mapper.imap_sequences(myjobs)
+                    for map_res in it:
+                        if map_res.target.hog_family_nr != 0:
+                            hog_id = db.format_hogid(map_res.target.hog_family_nr)
+                        else:
+                            hog_id = "n/a"
+                        csv_writer.writerow([map_res.query, map_res.target.omaid,
+                                             map_res.target.entry_nr == map_res.closest_entry_nr,
+                                             hog_id, map_res.distance, map_res.score])
+                        last_id = map_res.query
+                except KeyboardInterrupt as e:
+                    logger.info("received KeyboardInterrupt: {}".format(e))
+                    logger.info("writing checkpoint")
+                    with open(outfn+'.ckpt', 'w') as ckpt:
+                        ckpt.write(last_id)
+                    logger.info("quiting with 99")
+                    if len(conf.fasta) > 1:
+                        logger.warning("checkpointing does only properly work with a single input file. You used more than one file. please investigate how to recover")
+                        sys.exit(98)
+                    sys.exit(99)

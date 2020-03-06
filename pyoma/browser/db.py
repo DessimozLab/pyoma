9352e565b286bb09fb8bcd07f5b7e3d038211956
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2020-03-06 14:53:03 +0100

    bump DB schema version

--- pyoma/browser/db.py
+++ pyoma/browser/db.py
@@ -116,7 +116,7 @@ class Database(object):
     """This is the main interface to the oma database. Queries
     will typically be issued by methods of this object. Typically
     the result of queries will be :py:class:`numpy.recarray` objects."""
-    EXPECTED_DB_SCHEMA = "3.2"
+    EXPECTED_DB_SCHEMA = "3.3"
 
     def __init__(self, db):
         if isinstance(db, str):

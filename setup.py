1799c7b37b65e939dda22e0a32a68782c6877731
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2020-07-31 12:46:43 +0200

    imporve setup and add README

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
-import sys
 import os
 import shutil
 
 name = "pyoma"
 
 req_packages = [
-    "numpy>=1.16",
-    "tables>=3.5.1",
+    "numpy >= 1.16",
+    "tables >= 3.5.1",
     "future",
-    "fuzzyset>=0.0.17",
+    "fuzzyset >= 0.0.17",
     "tqdm",
-    "pyopa>=0.8",
-    "pandas>=0.22",
-    "biopython",
+    "pyopa >= 0.8",
+    "pandas >= 0.22",
+    "biopython >= 1.76, == 1.76 ; python_version < 3.6",
     "datasketch",
 ]
-if sys.version_info < (3, 6):
-    req_packages.extend(["biopython<1.77"])
-if sys.version_info < (3, 3):
-    req_packages.extend(["mock", "functools32", "tables<3.6.0"])
+
+cur_dir = os.path.abspath(os.path.dirname(__file__))
 
 # Create oma2hdf to install
 shutil.copyfile("bin/importdata.py", "bin/oma2hdf")
@@ -29,20 +26,22 @@ for line in open("{}/__init__.py".format(name)):
     if line.startswith("__version__"):
         exec(line.strip())
 
+with open(os.path.join(cur_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name=name,
     version=__version__,
     author="Adrian Altenhoff",
     author_email="adrian.altenhoff@inf.ethz.ch",
-    description="todoc",
+    description="python library to interact and build OMA hdf5 files",
+    long_description=long_description,
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
@@ -55,7 +54,7 @@ setup(
     extras_require={
         "create_db": [
             "PySAIS",
-            "familyanalyzer>=0.6.0",
+            "familyanalyzer >= 0.6.0",
             "matplotlib",
             "scikit-learn",
             "scikit-fuzzy",
@@ -66,6 +65,7 @@ setup(
     dependency_links=[
         "git+ssh://gitolite@lab.dessimoz.org:2222/family-analyzer@master#egg=familyanalyzer"
     ],
+    python_requires=">=3.5",
 )
 
 # Remove local copy of oma2hdf (installed)

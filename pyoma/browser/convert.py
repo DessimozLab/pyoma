5d5015a67ff4d3a1a4b89dbfb6e44acd5aa5e2d9
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-01-06 12:10:51 +0100

    code to update only the crossreferences in hdf5

--- pyoma/browser/convert.py
+++ pyoma/browser/convert.py
@@ -1742,7 +1742,7 @@ class RootHOGMetaDataLoader(object):
     by :attr:`meta_data_path`, which defaults to /RootHOG/MetaData.
     """
 
-    keyword_name = "Keywords.drw"
+    keyword_name = "RootHOG_Keywords.drw"
     expected_keys = ["Keywords"]
     tab_description = tablefmt.RootHOGMetaTable
     meta_data_path = "/RootHOG/MetaData"

c2f12b5c586d6f97432755e2039edf7e69ad5ad8
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-12-08 15:50:45 +0100

    allow to specify an output file name for augmentation process

--- bin/augement_orthoxml.py
+++ bin/augement_orthoxml.py
@@ -7,7 +7,9 @@ import logging
 logger = logging.getLogger("augment_orthoxml")
 
 
-def augment_orthoxml(h5, taxonomy, orthoxml, release):
+def augment_orthoxml(h5, taxonomy, orthoxml, release, out=None):
+    if out is None:
+        out = orthoxml + ".augmented"
     entry_tab = h5.get_node("/Protein/Entries")
     tax_tab = h5.get_node("/Taxonomy")
     tax_2_code = {
@@ -26,6 +28,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Augment existing orthoxml file")
     parser.add_argument("--hdf5", required=True, help="Path to the hdf5 database")
     parser.add_argument("--orthoxml-file", required=True, help="input orthoxml file")
+    parser.add_argument("--out", help="path to output file. defaults to inputfile + '.augmented'")
     parser.add_argument(
         "--taxonomy", required=True, help="Taxonomy file in newick format"
     )
@@ -40,4 +43,4 @@ if __name__ == "__main__":
     logger.info("Params: {}".format(conf))
 
     with tables.open_file(conf.hdf5, "r") as h5:
-        augment_orthoxml(h5, conf.taxonomy, conf.orthoxml_file, conf.release)
+        augment_orthoxml(h5, conf.taxonomy, conf.orthoxml_file, conf.release, conf.out)

8ebb827195a9fe84924fd09cea9359ea849d5570
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2024-10-29 17:15:53 +0100

    [FIX] minor fixes

--- pyoma/browser/build/xref/__init__.py
+++ pyoma/browser/build/xref/__init__.py
@@ -1,3 +1,3 @@
 from .fetch import fetch
 from .filter import ChunkWriter, filter_records_on_taxids, load_relevant_taxids
-from .map import map_xrefs, collect_crossrefs
+from .map import map_xrefs, collect_crossrefs, combine_xrefs
